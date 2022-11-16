5c248b865ec9bf6648fd88bb7606e77db5621a5f
Crackedmind0001 <keshavyadav191302075@gmail.com>
2022-11-16 22:32:45 +0530

    Hello added.

--- app/src/main/java/com/keshav/homeui2/MainActivity.kt
+++ app/src/main/java/com/keshav/homeui2/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.keshav.homeui2.ui.theme.HomeUI2Theme
 
+// Hello
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
